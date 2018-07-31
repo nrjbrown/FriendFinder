92dafa07942ffb030771842fe6c0711a610e0956
Blockchain Dev <nevillebrown_930@hotmail.com>
2018-07-30 20:40:26 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,87 @@
+var friends = [
+    {
+        name: "Deadpool", 
+        photo:"https://media.giphy.com/media/IfWTaMHg31a80/giphy.gif",
+        scores:[
+        "5",
+        "5",
+        "4",
+        "4",
+        "5",
+        "5",
+        "5",
+        "5",
+        "4",
+        "5", 
+        ]
+    },
+
+    {
+        name: "Betty", 
+        photo:"https://media.giphy.com/media/fHr6SanEdFx8LBF4Dv/giphy.gif",
+        scores:[
+        "1",
+        "2",
+        "4",
+        "1",
+        "2",
+        "1",
+        "1",
+        "5",
+        "4",
+        "1", 
+        ]
+    },
+
+    {
+        name: "Arnold", 
+        photo:"https://media.giphy.com/media/3o6nUQgYhSBUAXBOQo/giphy.gif",
+        scores:[
+        "2",
+        "3",
+        "4",
+        "2",
+        "3",
+        "1",
+        "2",
+        "3",
+        "4",
+        "1", 
+        ]
+    },
+
+    {
+        name: "Nicki", 
+        photo:"https://media.giphy.com/media/IfWTaMHg31a80/giphy.gif",
+        scores:[
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "5", 
+        ]
+    },
+    {
+        name: "Jenny", 
+        photo:"https://media.giphy.com/media/aQxn3drn43chG/giphy.gif",
+        scores:[
+        "2",
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3", 
+        ]
+    },
+];
+
+module.exports = friends;
